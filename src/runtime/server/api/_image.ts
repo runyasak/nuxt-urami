8cd48b1ba1172042f51713b3a81a6c8a245f44b1
Runyasak Chaengnaimuang <runyasak.c@gmail.com>
2023-12-09 21:26:53 +0700

    feat: add defaultStorePath

--- src/runtime/server/api/_image.ts
+++ src/runtime/server/api/_image.ts
@@ -2,12 +2,17 @@ import { createRequestHandler } from "@urami/core";
 import { defineEventHandler } from "h3";
 import type { HandlerConfig } from "../../../module";
 
+const defaultStorePath = ".nuxt-urami/images";
+
 export default defineEventHandler((event) => {
   const handlerConfig = process.env["handlerConfig"]
     ? (JSON.parse(process.env["handlerConfig"]) as HandlerConfig)
     : undefined;
 
-  const handler = createRequestHandler(handlerConfig);
+  const handler = createRequestHandler({
+    ...handlerConfig,
+    storePath: handlerConfig?.storePath || defaultStorePath,
+  });
 
   const url = event.node.req.headers.host + (event.node.req.originalUrl || "");
 
